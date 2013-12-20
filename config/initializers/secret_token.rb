ca4b08211db79fada1fb16f63630ecd4a939e4ec
SomethingWitty <tsai.stephen@gmail.com>
2013-12-19 22:02:39 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Email::Application.config.secret_key_base = '8332444989dd43899d6ad26b347d9557e9b6a585070d9a92db7f87b1e578403ba7bce8115cc1213231449964b1a9060162abdc936a6f29dae2eedd4609074f43'
