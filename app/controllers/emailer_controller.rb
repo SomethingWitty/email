54a56fbb0807f7eead3b9122ba7bf8155cdf20d4
SomethingWitty <tsai.stephen@gmail.com>
2013-12-23 10:06:12 -0800

    strong params

--- app/controllers/emailer_controller.rb
+++ app/controllers/emailer_controller.rb
@@ -2,6 +2,9 @@ require 'mail'
 
 class EmailerController < ApplicationController
   def send_email
+		params.require("subject")
+		params.require("to")
+		params.require("body")
 		Notifier.send_email(params["to"], params["subject"], params["body"]).deliver
   end
 end
